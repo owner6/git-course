0dba46b254e0fb1fa128f6d29eae1274176109af
onyshchenko-vladyslav <owner1147@gmail.com>
2022-09-03 02:22:07 +0300

    remove duplicated tsonsole.log

--- scrypt.js
+++ scrypt.js
@@ -1,6 +1,2 @@
 document.body.innerHTML = '<h1>hello js</h1>';
-console.log('test1')
-console.log('test2!!!!!!!!!!')
-console.log('test2!!!!!!!!!!')
-console.log('test3')
-console.log('test45')
+console.log('test1');
